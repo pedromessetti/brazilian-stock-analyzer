ef95e1929ca4e1894730ec4594404bebf42a91e3
Pedro Messetti <105685220+pedromessetti@users.noreply.github.com>
2023-05-19 09:28:51 +0100

    Update csv-writer.py

--- csv-writer.py
+++ csv-writer.py
@@ -1,29 +1,28 @@
-from fpdf import FPDF
 from bs4 import BeautifulSoup
 import requests
+import csv
 
 URL = "https://fundamentus.com.br/resultado.php"
-headers = {'User-Agent': "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36"}
+headers = {'User-Agent':"Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36"}
 site = requests.get(URL, headers=headers)
 soup = BeautifulSoup(site.content, 'html.parser')
 
-pdf_file = 'tabela.pdf'
-
-pdf = FPDF()
-pdf.add_page()
-pdf.set_font('Arial', 'B', 16)
+file = 'tabela.csv'
+csv_writer = csv.writer(open(file, 'w'))
 
 for tr in soup.find_all('tr'):
-    th_tags = [th.text for th in tr.find_all('th')]
-    td_tags = [td.text.strip() for td in tr.find_all('td')]
-
-    if th_tags:
-        for item in th_tags:
-            pdf.cell(40, 10, str(item))
-        pdf.ln()
-    elif td_tags:
-        for item in td_tags:
-            pdf.cell(40, 10, str(item))
-        pdf.ln()
-
-pdf.output(pdf_file, 'F')
+    data = []
+    
+    for th in tr.find_all('th'):
+        data.append(th.text)
+    
+    if data:
+        csv_writer.writerow(data)
+        continue
+    
+    for td in tr.find_all('td'):
+        data.append(td.text.strip())
+        
+    if data:
+        csv_writer.writerow(data)
+    
